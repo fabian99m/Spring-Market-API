6c3fb9a5a6ec185b2cd186e4c9aec0d88e9582f3
Fabianm99 <fabianjose99@gmail.com>
2021-08-12 12:07:49 -0500

    ProductoRepository ok

---
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -0,0 +1,41 @@
+package com.platzi.market.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.platzi.market.persistence.crud.ProductoCrudRepository;
+import com.platzi.market.persistence.entity.Producto;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.FindByIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscazos(int cantidadStock) {
+
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+}
