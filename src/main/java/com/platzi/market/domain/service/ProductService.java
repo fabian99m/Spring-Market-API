93f06b0faa8cc55a8116de04f621977b40b30d40
Fabianm99 <fabianjose99@gmail.com>
2021-08-13 14:10:35 -0500

    all request objects ok

---
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.platzi.market.domain.service;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProducto(int idProducto) {
+        return productRepository.getProduct(idProducto);
+    }
+
+    public Optional<List<Product>> getByCategoria(int idCategoria) {
+        return productRepository.getByCategory(idCategoria);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public boolean delete(int idProducto) {
+        if (productRepository.productExist(idProducto)) {
+            productRepository.delete(idProducto);
+            return true;
+        } else return false;
+    }
+
+}
